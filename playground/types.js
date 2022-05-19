00eeab1532f46ffc71a21a1667a873851062ef74
Alex Morton <alexmorton_dev@protonmail.com>
2022-05-19 21:20:41 +0300

    start journey

---
+++ playground/types.js
@@ -0,0 +1,41 @@
+// const mytext = 'text';
+// console.log(mytext);
+
+// const text = 'sds';
+
+// console.log(text);
+
+// const expample = 'dsds';
+
+// const boss = `${expample}`;
+
+// console.log(boss);
+
+// const newHeader = `Hi I'm John`;
+// const paragraph = `I'm a paragraph`;
+
+// const html = `
+// <div>
+// <h1>${newHeader}</h1>
+// <p>${paragraph}</p>
+// </div>
+
+// `;
+
+// document.body.innerHTML = html;
+
+const age = 23;
+
+const smarties = 20;
+
+const kids = 3;
+const dadGets = smarties % kids;
+const eachKidgets = Math.floor(smarties / kids);
+
+console.log(eachKidgets);
+
+const price = 1034;
+
+// sybmols
+
+// SNOBSN
