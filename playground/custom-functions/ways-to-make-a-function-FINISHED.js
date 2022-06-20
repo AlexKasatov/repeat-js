4f775c7dcdcaca46a548063999c0484840173a91
Alex Morton <alexmorton_dev@protonmail.com>
2022-06-20 21:50:02 +0300

    refresh promises

--- playground/custom-functions/ways-to-make-a-function-FINISHED.js
+++ playground/custom-functions/ways-to-make-a-function-FINISHED.js
@@ -13,7 +13,7 @@
 // };
 
 /* eslint-disable */
-const inchToCM = inches => inches * 2.54;
+const inchToCM = (inches) => inches * 2.54;
 
 // function add(a, b = 3) {
 //   const total = a + b;
@@ -34,47 +34,44 @@ const inchToCM = inches => inches * 2.54;
 
 const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
 
-
 // IIFE
 // Immediately Invoked Function Expression
 
-(function(age) {
+(function (age) {
   return `You are cool and age ${age}`;
 })(10);
 
 // Methods!!!
 const wes = {
-  name: 'Westopher Bos',
+  name: "Westopher Bos",
   // Method!
-  sayHi: function() {
+  sayHi: function () {
     console.log(`Hey ${this.name}`);
-    return 'Hey Wes';
+    return "Hey Wes";
   },
   // Short hand Method
   yellHi() {
-    console.log('HEY WESSSSS');
+    console.log("HEY WESSSSS");
   },
   // Arrow function
   wisperHi: () => {
-    console.log('hii wesss im a mouse');
-  }
-}
+    console.log("hii wesss im a mouse");
+  },
+};
 
 // Callback Functions
 // Click Callback
-const button = document.querySelector('.clickMe');
+const button = document.querySelector(".clickMe");
 
 function handleClick() {
-  console.log('Great Clicking!!');
+  console.log("Great Clicking!!");
 }
 
-button.addEventListener('click', function() {
-  console.log('NIce Job!!!');
+button.addEventListener("click", function () {
+  console.log("NIce Job!!!");
 });
 
 // Timer Callback
 setTimeout(() => {
-  console.log('DONE! Time to eat!');
-}, 1000)
-
-
+  console.log("DONE! Time to eat!");
+}, 1000);
